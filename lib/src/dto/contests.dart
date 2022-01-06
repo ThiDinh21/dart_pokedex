53ce8f62a13ec77d9a2d3787794be98e687c3fb6
Thi Dinh <vietthidinh2001@gmail.com>
2022-01-07 00:27:25 +0200

    doc: add for dto/utility/contests.dart

--- lib/src/dto/contests.dart
+++ lib/src/dto/contests.dart
@@ -1,6 +1,9 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:poke_dart/src/aliases.dart';
+import 'package:poke_dart/src/dto/berries.dart';
+import 'package:poke_dart/src/dto/moves.dart';
 import 'package:poke_dart/src/dto/utility/common.dart';
+import 'package:poke_dart/src/dto/utility/languages.dart';
 
 part 'contests.freezed.dart';
 part 'contests.g.dart';
@@ -8,9 +11,20 @@ part 'contests.g.dart';
 @freezed
 class ContestType with _$ContestType {
   const factory ContestType(
+    /// The identifier for this resource.
     int id,
+
+    /// The name for this resource.
     String name,
+
+    /// The berry flavor that correlates with this contest type.
+    ///
+    /// See also:
+    ///
+    /// [BerryFlavor]
     @JsonKey(name: 'berry_flavor') NamedApiResource berryFlavor,
+
+    /// The name of this contest type listed in different languages.
     List<ContestName> names,
   ) = _ContestType;
 
@@ -20,8 +34,17 @@ class ContestType with _$ContestType {
 @freezed
 class ContestName with _$ContestName {
   const factory ContestName(
+    /// The name for this contest.
     String name,
+
+    /// The color associated with this contest's name.
     String color,
+
+    /// The language that this name is in.
+    ///
+    /// See also:
+    ///
+    /// [Language]
     NamedApiResource language,
   ) = _ContestName;
 
@@ -31,10 +54,19 @@ class ContestName with _$ContestName {
 @freezed
 class ContestEffect with _$ContestEffect {
   const factory ContestEffect(
+    /// The identifier for this resource.
     int id,
+
+    /// The base number of hearts the user of this move gets.
     int appeal,
+
+    /// The base number of hearts the user's opponent loses.
     int jam,
+
+    /// The result of this contest effect listed in different languages.
     @JsonKey(name: 'effect_entries') List<Effect> effectEntries,
+
+    /// The flavor text of this contest effect listed in different languages.
     @JsonKey(name: 'flavor_text_entries') List<FlavorText> flavorTextEntries,
   ) = _ContestEffect;
 
@@ -44,9 +76,20 @@ class ContestEffect with _$ContestEffect {
 @freezed
 class SuperContestEffect with _$SuperContestEffect {
   const factory SuperContestEffect(
+    /// The identifier for this resource.
     int id,
+
+    /// The level of appeal this super contest effect has.
     int appeal,
+
+    /// The flavor text of this super contest effect listed in different languages.
     @JsonKey(name: 'flavor_text_entries') List<FlavorText> flavorTextEntries,
+
+    /// A list of moves that have the effect when used in super contests.
+    ///
+    /// See also:
+    ///
+    /// [Move]
     List<NamedApiResource> moves,
   ) = _SuperContestEffect;
 
