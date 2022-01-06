36afc54674d6b2fef3acc251d20d6287d21ca0f3
Thi Dinh <vietthidinh2001@gmail.com>
2022-01-07 00:10:29 +0200

    doc: add for dto/utility/languages.dart

--- lib/src/dto/utility/languages.dart
+++ lib/src/dto/utility/languages.dart
@@ -10,11 +10,22 @@ part 'languages.g.dart';
 @freezed
 class Language with _$Language {
   const factory Language(
+    /// The identifier for this resource.
     int id,
+
+    /// The name for this resource.
     String name,
+
+    /// Whether or not the games are published in this language.
     bool official,
+
+    /// The two-letter code of the country where this language is spoken. Note that it is not unique.
     String iso639,
+
+    /// The two-letter code of the language. Note that it is not unique.
     String iso3166,
+
+    /// The name of this resource listed in different languages.
     List<Name> names,
   ) = _Language;
 
