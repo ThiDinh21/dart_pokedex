8356be243642dde16aad1b29603cae9f2015e227
Thi Dinh <vietthidinh2001@gmail.com>
2022-01-07 12:56:13 +0200

    doc: add for dto/games.dart

--- lib/src/dto/games.dart
+++ lib/src/dto/games.dart
@@ -2,6 +2,9 @@
 
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:poke_dart/src/aliases.dart';
+import 'package:poke_dart/src/dto/locations.dart';
+import 'package:poke_dart/src/dto/moves.dart';
+import 'package:poke_dart/src/dto/pokemon.dart';
 import 'package:poke_dart/src/dto/utility/common.dart';
 
 part 'games.freezed.dart';
@@ -10,14 +13,55 @@ part 'games.g.dart';
 @freezed
 class Generation with _$Generation {
   const factory Generation(
+    /// The identifier for this resource.
     int id,
+
+    /// The name for this resource.
     String name,
+
+    /// A list of abilities that were introduced in this generation.
+    ///
+    /// See also:
+    ///
+    /// [Ability]
     List<NamedApiResource> abilities,
+
+    /// The name of this resource listed in different languages.
     List<Name> names,
+
+    /// The main region travelled in this generation.
+    ///
+    /// See also:
+    ///
+    /// [Region]
     @JsonKey(name: 'main_region') NamedApiResource mainRegion,
+
+    /// A list of moves that were introduced in this generation.
+    ///
+    /// See also:
+    ///
+    /// [Move]
     List<NamedApiResource> moves,
+
+    /// A list of Pokémon species that were introduced in this generation.
+    ///
+    /// See also:
+    ///
+    /// [PokemonSpecies]
     @JsonKey(name: 'pokemon_species') List<NamedApiResource> pokemonSpecies,
+
+    /// A list of types that were introduced in this generation.
+    ///
+    /// See also:
+    ///
+    /// [Type]
     List<NamedApiResource> types,
+
+    /// A list of version groups that were introduced in this generation.
+    ///
+    /// See also:
+    ///
+    /// [VersionGroup]
     @JsonKey(name: 'version_groups') List<NamedApiResource> versionGroups,
   ) = _Generation;
 
@@ -27,13 +71,36 @@ class Generation with _$Generation {
 @freezed
 class PokedexData with _$PokedexData {
   const factory PokedexData(
+    /// The identifier for this resource.
     int id,
+
+    /// The name for this resource.
     String name,
+
+    /// Whether or not this Pokédex originated in the main series of the video games.
     @JsonKey(name: 'is_main_series') bool isMainSeries,
+
+    /// The description of this resource listed in different languages.
     List<Description> descriptions,
+
+    /// The name of this resource listed in different languages.
     List<Name> names,
+
+    /// A list of Pokémon catalogued in this Pokédex and their indexes.
     @JsonKey(name: 'pokemon_entries') List<PokemonEntry> pokemonEntries,
+
+    /// The region this Pokédex catalogues Pokémon for.
+    ///
+    /// See also:
+    ///
+    /// [Region]
     NamedApiResource region,
+
+    /// A list of version groups this Pokédex is relevant to.
+    ///
+    /// See also:
+    ///
+    /// [VersionGroup]
     @JsonKey(name: 'version_groups') List<NamedApiResource> versionGroups,
   ) = _PokedexData;
 
@@ -43,7 +110,14 @@ class PokedexData with _$PokedexData {
 @freezed
 class PokemonEntry with _$PokemonEntry {
   const factory PokemonEntry(
+    /// The index of this Pokémon species entry within the Pokédex.
     @JsonKey(name: 'entry_number') int entryNumber,
+
+    /// The Pokémon species being encountered.
+    ///
+    /// See also:
+    ///
+    /// [PokemonSpecies]
     @JsonKey(name: 'pokemon_species') NamedApiResource pokemonSpecies,
   ) = _PokemonEntry;
 
@@ -53,9 +127,20 @@ class PokemonEntry with _$PokemonEntry {
 @freezed
 class Version with _$Version {
   const factory Version(
+    /// The identifier for this resource.
     int id,
+
+    /// The name for this resource.
     String name,
+
+    /// The name of this resource listed in different languages.
     List<Name> names,
+
+    /// The version group this version belongs to.
+    ///
+    /// See also:
+    ///
+    /// [VersionGroup]
     @JsonKey(name: 'version_group') NamedApiResource versionGroup,
   ) = _Version;
 
@@ -65,14 +150,50 @@ class Version with _$Version {
 @freezed
 class VersionGroup with _$VersionGroup {
   const factory VersionGroup(
+    /// The identifier for this resource.
     int id,
+
+    /// The name for this resource.
     String name,
+
+    /// Order for sorting.
+    /// Almost by date of release, except similar versions are grouped together.
     int order,
+
+    /// The generation this version was introduced in.
+    ///
+    /// See also:
+    ///
+    /// [Generation]
     NamedApiResource generation,
+
+    /// A list of methods in which Pokémon can learn moves in this version group.
+    ///
+    /// See also:
+    ///
+    /// [MoveLearnMethod]
     @JsonKey(name: 'move_learn_methods')
         List<NamedApiResource> moveLearnMethods,
+
+    /// A list of Pokédexes introduces in this version group.
+    ///
+    /// See also:
+    ///
+    /// [PokedexData]
     List<NamedApiResource> pokedexes,
+
+    /// A list of regions that can be visited in this version group.
+    ///
+    /// See also:
+    ///
+    /// [Region]
     List<NamedApiResource> regions,
+
+    /// The versions this version group owns.
+    ///
+    /// See also:
+    ///
+    /// [Version]
     List<NamedApiResource> versions,
   ) = _VersionGroup;
 
