68f15ac9237c069d44bcf9df79e67ce98ccb0f47
Thi Dinh <vietthidinh2001@gmail.com>
2022-01-11 08:55:15 +0200

    doc: add for dto/machines.dart

--- lib/src/dto/machines.dart
+++ lib/src/dto/machines.dart
@@ -1,5 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:poke_dart/src/aliases.dart';
+import 'package:poke_dart/src/dto/items.dart';
+import 'package:poke_dart/src/dto/moves.dart';
 import 'package:poke_dart/src/dto/utility/common.dart';
 
 part 'machines.freezed.dart';
@@ -8,9 +10,28 @@ part 'machines.g.dart';
 @freezed
 class Machine with _$Machine {
   const factory Machine(
+    /// The identifier for this resource.
     int id,
+
+    /// The TM or HM item that corresponds to this machine.
+    ///
+    /// See also:
+    ///
+    /// [Item]
     NamedApiResource item,
+
+    /// The move that is taught by this machine.
+    ///
+    /// See also:
+    ///
+    /// [Move]
     NamedApiResource move,
+
+    /// The version group that this machine applies to.
+    ///
+    /// See also:
+    ///
+    /// [VersionGroup]
     @JsonKey(name: 'version_group') NamedApiResource versionGroup,
   ) = _Machine;
 
