67326b9b9f1fa627ce58438215ed3329e376233f
Thi Dinh <vietthidinh2001@gmail.com>
2022-01-12 21:05:12 +0200

    doc: add for dto/moves

--- lib/src/dto/moves.dart
+++ lib/src/dto/moves.dart
@@ -4,6 +4,7 @@ import 'package:poke_dart/src/dto/contests.dart';
 import 'package:poke_dart/src/dto/games.dart';
 import 'package:poke_dart/src/dto/pokemon.dart';
 import 'package:poke_dart/src/dto/utility/common.dart';
+import 'package:poke_dart/src/dto/utility/languages.dart';
 
 part 'moves.freezed.dart';
 part 'moves.g.dart';
@@ -124,7 +125,12 @@ class Move with _$Move {
 @freezed
 class ContestComboSets with _$ContestComboSets {
   const factory ContestComboSets(
+    /// A detail of moves this move can be used before or after,
+    /// granting additional appeal points in contests.
     ContestComboDetail normal,
+
+    /// A detail of moves this move can be used before or after,
+    /// granting additional appeal points in super contests.
     @JsonKey(name: 'super') ContestComboDetail superMove,
   ) = _ContestComboSets;
 
@@ -135,7 +141,10 @@ class ContestComboSets with _$ContestComboSets {
 @freezed
 class ContestComboDetail with _$ContestComboDetail {
   const factory ContestComboDetail(
+    /// A list of moves to use before this move.
     @JsonKey(name: 'use_before') List<NamedApiResource> useBefore,
+
+    /// A list of moves to use after this move.
     @JsonKey(name: 'use_after') List<NamedApiResource> useAfter,
   ) = _ContestComboDetail;
 
@@ -146,8 +155,21 @@ class ContestComboDetail with _$ContestComboDetail {
 @freezed
 class MoveFlavorText with _$MoveFlavorText {
   const factory MoveFlavorText(
+    /// The localized flavor text for an api resource in a specific language.
     @JsonKey(name: 'flavor_text') String flavorText,
+
+    /// The language this name is in.
+    ///
+    /// See also:
+    ///
+    /// [Language]
     NamedApiResource language,
+
+    /// The version group that uses this flavor text.
+    ///
+    /// See also:
+    ///
+    /// [VersionGroup]
     @JsonKey(name: 'version_group') NamedApiResource versionGroup,
   ) = _MoveFlavorText;
 
@@ -157,17 +179,50 @@ class MoveFlavorText with _$MoveFlavorText {
 @freezed
 class MoveMetaData with _$MoveMetaData {
   const factory MoveMetaData(
+    /// The status ailment this move inflicts on its target.
+    ///
+    /// See also:
+    ///
+    /// [MoveAilment]
     NamedApiResource ailment,
+
+    /// The category of move this move falls under, e.g. damage or ailment.
+    ///
+    /// See also:
+    ///
+    /// [MoveCategory]
     NamedApiResource category,
+
+    /// The minimum number of times this move hits. Null if it always only hits once.
     @JsonKey(name: 'min_hits') int minHits,
+
+    /// The maximum number of times this move hits. Null if it always only hits once.
     @JsonKey(name: 'max_hits') int maxHits,
+
+    /// The minimum number of turns this move continues to take effect.
+    /// Null if it always only lasts one turn.
     @JsonKey(name: 'min_turns') int minTurns,
+
+    /// The maximum number of turns this move continues to take effect.
+    /// Null if it always only lasts one turn.
     @JsonKey(name: 'max_turns') int maxTurns,
+
+    /// HP drain (if positive) or Recoil damage (if negative), in percent of damage done.
     int drain,
+
+    /// The amount of hp gained by the attacking Pokemon, in percent of it's maximum HP.
     int healing,
+
+    /// Critical hit rate bonus.
     @JsonKey(name: 'crit_rate') int critRate,
+
+    /// The likelihood this attack will cause an ailment.
     @JsonKey(name: 'ailment_chance') int ailmentChance,
+
+    /// The likelihood this attack will cause the target Pokémon to flinch.
     @JsonKey(name: 'flinch_chance') int flinchChance,
+
+    /// The likelihood this attack will cause a stat change in the target Pokémon.
     @JsonKey(name: 'stat_chance') int statChance,
   ) = _MoveMetaData;
 
@@ -177,7 +232,14 @@ class MoveMetaData with _$MoveMetaData {
 @freezed
 class MoveStatChange with _$MoveStatChange {
   const factory MoveStatChange(
+    /// The amount of change.
     int change,
+
+    /// The stat being affected.
+    ///
+    /// See also:
+    ///
+    /// [Stat]
     NamedApiResource stat,
   ) = _MoveStatChange;
 
@@ -187,12 +249,33 @@ class MoveStatChange with _$MoveStatChange {
 @freezed
 class PastMoveStatValues with _$PastMoveStatValues {
   const factory PastMoveStatValues(
+    /// The percent value of how likely this move is to be successful.
     int accuracy,
+
+    /// The percent value of how likely it is this moves effect will take effect.
     @JsonKey(name: 'effect_chance') int effectChance,
+
+    /// The base power of this move with a value of 0 if it does not have a base power.
     int power,
+
+    /// Power points. The number of times this move can be used.
     int pp,
+
+    /// The effect of this move listed in different languages.
     @JsonKey(name: 'effect_entries') List<VerboseEffect> effectEntries,
+
+    /// The elemental type of this move.
+    ///
+    /// See also:
+    ///
+    /// [Type]
     NamedApiResource type,
+
+    /// The version group in which these move stat values were in effect.
+    ///
+    /// See also:
+    ///
+    /// [VersionGroup]
     @JsonKey(name: 'version_group') NamedApiResource versionGroup,
   ) = _PastMoveStatValues;
 
@@ -203,9 +286,20 @@ class PastMoveStatValues with _$PastMoveStatValues {
 @freezed
 class MoveAilment with _$MoveAilment {
   const factory MoveAilment(
+    /// The identifier for this resource.
     int id,
+
+    /// The name for this resource.
     String name,
+
+    /// A list of moves that cause this ailment.
+    ///
+    /// See also:
+    ///
+    /// [Move]
     List<NamedApiResource> moves,
+
+    /// The name of this resource listed in different languages.
     List<Name> names,
   ) = _MoveAilment;
 
@@ -215,8 +309,13 @@ class MoveAilment with _$MoveAilment {
 @freezed
 class MoveBattleStyle with _$MoveBattleStyle {
   const factory MoveBattleStyle(
+    /// The identifier for this resource.
     int id,
+
+    /// The name for this resource.
     String name,
+
+    /// The name of this resource listed in different languages.
     List<Name> names,
   ) = _MoveBattleStyle;
 
@@ -227,9 +326,20 @@ class MoveBattleStyle with _$MoveBattleStyle {
 @freezed
 class MoveCategory with _$MoveCategory {
   const factory MoveCategory(
+    /// The identifier for this resource.
     int id,
+
+    /// The name for this resource.
     String name,
+
+    /// A list of moves that fall into this category.
+    ///
+    /// See also:
+    ///
+    /// [Move]
     List<NamedApiResource> moves,
+
+    /// The description of this resource listed in different languages.
     List<Description> descriptions,
   ) = _MoveCategory;
 
@@ -239,10 +349,23 @@ class MoveCategory with _$MoveCategory {
 @freezed
 class MoveDamageClass with _$MoveDamageClass {
   const factory MoveDamageClass(
+    /// The identifier for this resource.
     int id,
+
+    /// The name for this resource.
     String name,
+
+    /// The description of this resource listed in different languages.
     List<Description> descriptions,
+
+    /// A list of moves that fall into this damage class.
+    ///
+    /// See also:
+    ///
+    /// [Move]
     List<NamedApiResource> moves,
+
+    /// The name of this resource listed in different languages.
     List<Name> names,
   ) = _MoveDamageClass;
 
@@ -253,10 +376,23 @@ class MoveDamageClass with _$MoveDamageClass {
 @freezed
 class MoveLearnMethod with _$MoveLearnMethod {
   const factory MoveLearnMethod(
+    /// The identifier for this resource.
     int id,
+
+    /// The name for this resource.
     String name,
+
+    /// The description of this resource listed in different languages.
     List<Description> descriptions,
+
+    /// The name of this resource listed in different languages.
     List<Name> names,
+
+    /// A list of version groups where moves can be learned through this method.
+    ///
+    /// See also:
+    ///
+    /// [VersionGroup]
     @JsonKey(name: 'version_groups') List<NamedApiResource> versionGroups,
   ) = _MoveLearnMethod;
 
@@ -267,10 +403,23 @@ class MoveLearnMethod with _$MoveLearnMethod {
 @freezed
 class MoveTarget with _$MoveTarget {
   const factory MoveTarget(
+    /// The identifier for this resource.
     int id,
+
+    /// The name for this resource.
     String name,
+
+    /// The description of this resource listed in different languages.
     List<Description> descriptions,
+
+    /// A list of moves that that are directed at this target.
+    ///
+    /// See also:
+    ///
+    /// [Move]
     List<NamedApiResource> moves,
+
+    /// The name of this resource listed in different languages.
     List<Name> names,
   ) = _MoveTarget;
 
