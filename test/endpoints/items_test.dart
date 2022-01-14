5e8234bc2bf4413cd406f09083d22d4969161766
Thi Dinh <vietthidinh2001@gmail.com>
2022-01-14 15:46:16 +0200

    test: Items endpoints

--- test/endpoints/items_test.dart
+++ test/endpoints/items_test.dart
@@ -42,4 +42,124 @@ void main() {
       expect(items.length, 20);
     });
   });
+
+  group('ItemAttribute', () {
+    final List<ItemAttribute> attribs = [];
+    late BaseNamedEndpoint<ItemAttribute> endpoint;
+
+    setUp(() {
+      attribs.clear();
+      endpoint = dex.itemAttributes;
+    });
+
+    test('getAll', () async {
+      final resources = await endpoint.getAll();
+      for (final resource in resources.results) {
+        final item = await endpoint.getByUrl(resource.url);
+        attribs.add(item);
+      }
+
+      expect(attribs.length, 8);
+    });
+
+    test('getPage', () async {
+      final resources = await endpoint.getPage();
+      for (final resource in resources.results) {
+        final item = await endpoint.getByUrl(resource.url);
+        attribs.add(item);
+      }
+
+      expect(attribs.length, 8);
+    });
+  });
+
+  group('ItemCategory', () {
+    final List<ItemCategory> categories = [];
+    late BaseNamedEndpoint<ItemCategory> endpoint;
+
+    setUp(() {
+      categories.clear();
+      endpoint = dex.itemCategories;
+    });
+
+    test('getAll', () async {
+      final resources = await endpoint.getAll();
+      for (final resource in resources.results) {
+        final item = await endpoint.getByUrl(resource.url);
+        categories.add(item);
+      }
+
+      expect(categories.length, 50);
+    });
+
+    test('getPage', () async {
+      final resources = await endpoint.getPage();
+      for (final resource in resources.results) {
+        final item = await endpoint.getByUrl(resource.url);
+        categories.add(item);
+      }
+
+      expect(categories.length, 20);
+    });
+  });
+
+  group('ItemFlingEffect', () {
+    final List<ItemFlingEffect> flingEffs = [];
+    late BaseNamedEndpoint<ItemFlingEffect> endpoint;
+
+    setUp(() {
+      flingEffs.clear();
+      endpoint = dex.itemFlingEffects;
+    });
+
+    test('getAll', () async {
+      final resources = await endpoint.getAll();
+      for (final resource in resources.results) {
+        final item = await endpoint.getByUrl(resource.url);
+        flingEffs.add(item);
+      }
+
+      expect(flingEffs.length, 7);
+    });
+
+    test('getPage', () async {
+      final resources = await endpoint.getPage();
+      for (final resource in resources.results) {
+        final item = await endpoint.getByUrl(resource.url);
+        flingEffs.add(item);
+      }
+
+      expect(flingEffs.length, 7);
+    });
+  });
+
+  group('ItemPocket', () {
+    final List<ItemPocket> itemPockets = [];
+    late BaseNamedEndpoint<ItemPocket> endpoint;
+
+    setUp(() {
+      itemPockets.clear();
+      endpoint = dex.itemPockets;
+    });
+
+    test('getAll', () async {
+      final resources = await endpoint.getAll();
+      for (final resource in resources.results) {
+        final item = await endpoint.getByUrl(resource.url);
+        itemPockets.add(item);
+      }
+
+      expect(itemPockets.length, 8);
+    });
+
+    test('getPage', () async {
+      final resources = await endpoint.getPage();
+      for (final resource in resources.results) {
+        final item = await endpoint.getByUrl(resource.url);
+        itemPockets.add(item);
+      }
+
+      expect(itemPockets.length, 8);
+    });
+  });
 }
