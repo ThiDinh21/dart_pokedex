cc7d44ed657d01346c99317ebe5d9165493fc66f
Thi Dinh <vietthidinh2001@gmail.com>
2022-01-24 02:45:36 +0200

    feat: add example

--- example/example.dart
+++ example/example.dart
@@ -1,10 +1,38 @@
-// ignore_for_file: unused_import, avoid_print, unused_local_variable, prefer_const_declarations
-
 import 'package:pokedex/pokedex.dart';
 
 Future<void> main() async {
   final pokedex = Pokedex();
-  final x = await pokedex.pokemon.get(id: 10);
-  final y = const APIResource('a');
-  print(x);
+
+  // with Future
+  pokedex.pokemonSpecies.get(name: "aegislash").then((aegislashSpecies) {
+    print(aegislashSpecies);
+  });
+
+  // with async function
+  final aegislashBlade = await pokedex.pokemon.get(name: "aegislash-blade");
+  print(aegislashBlade);
+
+  // get pokemon by id
+  final aegislashShield = await pokedex.pokemon.get(id: 681);
+  print(aegislashShield);
+
+  // get list of resources for pokemon
+  final pokemonSpeciesResources =
+      (await pokedex.pokemonSpecies.getPage(limit: 100)).results;
+  for (final resource in pokemonSpeciesResources) {
+    final species = await pokedex.pokemonSpecies.getByUrl(resource.url);
+    print(species);
+  }
+
+  // get all berries
+  final berryResources = (await pokedex.berries.getAll()).results;
+  for (final resource in berryResources) {
+    final berry = await pokedex.berries.getByUrl(resource.url);
+    print(berry);
+  }
+
+  // get by url
+  final item =
+      await pokedex.item.getByUrl('https://pokeapi.co/api/v2/item/100');
+  print(item);
 }
